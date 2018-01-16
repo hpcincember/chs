0081447e05ff029a46a204e7368d9a94cc27827d
Raf <rafael.maghari@rarejob.ph>
2018-01-16 17:35:50 +0800

    sample

--- resources/views/leader/index.blade.php
+++ resources/views/leader/index.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Leaders</div>
-    hi modification
+    hi modificationfff
                     <div class="panel-body">
 
                     </div>
